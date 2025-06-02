280f3bd49e828a03d1ab707671a206103411093b
SamEagle <samadeagle@yahoo.com>
2025-06-02 11:30:38 +0330

    Use ai_generator's funcs in app, /generate route is alsmot complete.

--- src/app.py
+++ src/app.py
@@ -1,16 +1,38 @@
+import logging
+
 from flask import Flask, request, jsonify
 
+from core.ai_generator import generate_blog_post, generate_mock_blog_post
 from config.cfg import settings
 
+DEBUG = settings.DEBUG
+MOCK_DATA = settings.MOCK_DATA
+logger = logging.getLogger(__name__)
 app = Flask(__name__)
 
 
 @app.route("/generate")
 def generate():
-    keyword = request.args.get("keyword", "default")
+    keyword = request.args.get("keyword")
+    if not keyword:
+        return jsonify({"message": "Missing 'keyword' parameter"}), 400
 
-    return jsonify({"message": f"You sent: {keyword}"})
+    try:
+        post = (
+            generate_mock_blog_post(keyword)
+            if MOCK_DATA
+            else generate_blog_post(keyword, api_key=settings.OPEN_AI_API_KEY)
+        )
+
+        return jsonify({"post": post})
+
+    except ValueError as ve:
+        return jsonify({"error": str(ve)}), 400
+
+    except Exception:
+        logger.exception("Unexpected error during blog post generation")
+        raise
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=DEBUG)
